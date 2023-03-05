ce8fbfdb6d701269eca6148e02ab9e9857cd1175
Sergey <shamkolovich95@mail.ru>
2023-03-05 15:21:29 +0300

    feat: start project settings

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  console.log(12)
+root.render(
+  <React.StrictMode>
+    <h1>dasdsd</h1>
+  </React.StrictMode>
+);
