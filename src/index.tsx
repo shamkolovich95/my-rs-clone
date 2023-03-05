a463748f9ec0499e51277b23328535cf9d06818e
Sergey <shamkolovich95@mail.ru>
2023-03-05 20:03:11 +0300

    feat: add rules to eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-  console.log(12)
+
 root.render(
-  <React.StrictMode>
-    <h1>dasdsd</h1>
-  </React.StrictMode>
+    <React.StrictMode>
+        <h1>dasdsd</h1>
+    </React.StrictMode>
 );
